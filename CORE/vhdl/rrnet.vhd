49d3f6b4226878f6644774a578902298f2013fd3
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2026-07-05 13:19:33 +0200

    rrnet: PacketPage WIP

--- CORE/vhdl/rrnet.vhd
+++ CORE/vhdl/rrnet.vhd
@@ -1,5 +1,14 @@
 -- ----------------------------------------------------------
 -- Description: Simulates an CS8900A ethernet chip
+--
+-- From documentation: https://www.mi.fu-berlin.de/inf/groups/ag-tech/projects/ScatterWeb/moduleComponents/EWS_CS8900.pdf
+-- PacketPage Address:
+-- 0000 - 0045 : Bus Interface Registers
+-- 0100 - 013F : Status and Control Registers
+-- 0140 - 014F : Initiate Transmit Registers
+-- 0150 - 015D : Address Filter Registers
+-- 0400        : Receive Frame Location
+-- 0A00        : Transmit Frame Location
 -- ----------------------------------------------------------
 
 library ieee;
@@ -22,26 +31,56 @@ end entity rrnet;
 
 architecture rtl of rrnet is
 
-  constant C_PP_PTR     : unsigned(7 downto 0)        := x"02";
-  constant C_PP_DATA_0  : unsigned(7 downto 0)        := x"04";
-  constant C_PP_DATA_1  : unsigned(7 downto 0)        := x"06";
-  constant C_RXTX_REG_0 : unsigned(7 downto 0)        := x"08";
-  constant C_RXTX_REG_1 : unsigned(7 downto 0)        := x"0A";
-  constant C_TX_CMD     : unsigned(7 downto 0)        := x"0C";
-  constant C_TX_LENGTH  : unsigned(7 downto 0)        := x"0E";
+  constant C_PP_PTR     : unsigned(7 downto 0)      := x"02";
+  constant C_PP_DATA_0  : unsigned(7 downto 0)      := x"04";
+  constant C_PP_DATA_1  : unsigned(7 downto 0)      := x"06";
+  constant C_RXTX_REG_0 : unsigned(7 downto 0)      := x"08";
+  constant C_RXTX_REG_1 : unsigned(7 downto 0)      := x"0A";
+  constant C_TX_CMD     : unsigned(7 downto 0)      := x"0C";
+  constant C_TX_LENGTH  : unsigned(7 downto 0)      := x"0E";
 
-  signal   pp_ptr     : std_logic_vector(15 downto 0) := (others => '0');
-  signal   pp_data_0  : std_logic_vector(15 downto 0) := (others => '0');
-  signal   pp_data_1  : std_logic_vector(15 downto 0) := (others => '0');
-  signal   rxtx_reg_0 : std_logic_vector(15 downto 0) := (others => '0');
-  signal   rxtx_reg_1 : std_logic_vector(15 downto 0) := (others => '0');
-  signal   tx_cmd     : std_logic_vector(15 downto 0) := (others => '0');
-  signal   tx_length  : std_logic_vector(15 downto 0) := (others => '0');
+  signal   pp_ptr     : unsigned(15 downto 0)       := (others => '0');
+  signal   pp_data_0  : unsigned(15 downto 0)       := (others => '0');
+  signal   pp_data_1  : unsigned(15 downto 0)       := (others => '0');
+  signal   rxtx_reg_0 : unsigned(15 downto 0)       := (others => '0');
+  signal   rxtx_reg_1 : unsigned(15 downto 0)       := (others => '0');
+  signal   tx_cmd     : unsigned(15 downto 0)       := (others => '0');
+  signal   tx_length  : unsigned(15 downto 0)       := (others => '0');
 
-  type     byte_array_type is array (natural range <>) of std_logic_vector(7 downto 0);
-  signal   packet_page : byte_array_type(0 to 4095)   := (others => (others => '0'));
+  signal   pp_we    : std_logic;
+  signal   pp_wrdat : std_logic_vector(15 downto 0);
+  signal   pp_rddat : std_logic_vector(15 downto 0);
+
+
+  -- This holds the entire 2k words of PacketPage memory.
+  type     byte_array_type is array (natural range <>) of std_logic_vector(15 downto 0);
+
+  -- This generates the reset-value of the 2k words PacketPage memory.
+
+  pure function get_packet_page_init return byte_array_type is
+    variable ret_v : byte_array_type(0 to 2047) := (others => (others => '0'));
+  begin
+    -- EISA registration number for Crystal Semiconductor
+    ret_v(0) := x"630E";
+    -- Product ID and Revision number
+    ret_v(1) := x"0700";
+    return ret_v;
+  end function get_packet_page_init;
+
+  signal   packet_page : byte_array_type(0 to 2047) := get_packet_page_init;
 
 begin
+
+  pp_proc : process (clk_i)
+  begin
+    -- Note: Deliberately using falling edge
+    if falling_edge(clk_i) then
+      if pp_we = '1' then
+        packet_page(to_integer(pp_ptr)) <= pp_wrdat;
+      end if;
+      pp_rddat <= packet_page(to_integer(pp_ptr));
+    end if;
+  end process pp_proc;
 
   fsm_proc : process (clk_i)
   begin
@@ -52,28 +91,34 @@ begin
           case unsigned(addr_i) is
 
             when C_PP_PTR =>
-              pp_ptr(7 downto 0) <= wr_data_i;
+              pp_ptr(7 downto 0) <= unsigned(wr_data_i);
 
             when C_PP_PTR + 1 =>
-              pp_ptr(15 downto 8) <= wr_data_i;
+              pp_ptr(15 downto 8) <= unsigned(wr_data_i);
+
+            when C_PP_DATA_0 =>
+              pp_data_0(7 downto 0) <= unsigned(wr_data_i);
+
+            when C_PP_DATA_0 + 1 =>
+              pp_data_0(15 downto 8) <= unsigned(wr_data_i);
 
             when C_RXTX_REG_0 =>
-              rxtx_reg_0(7 downto 0) <= wr_data_i;
+              rxtx_reg_0(7 downto 0) <= unsigned(wr_data_i);
 
             when C_RXTX_REG_0 + 1 =>
-              rxtx_reg_0(15 downto 8) <= wr_data_i;
+              rxtx_reg_0(15 downto 8) <= unsigned(wr_data_i);
 
             when C_TX_CMD =>
-              tx_cmd(7 downto 0) <= wr_data_i;
+              tx_cmd(7 downto 0) <= unsigned(wr_data_i);
 
             when C_TX_CMD + 1 =>
-              tx_cmd(15 downto 8) <= wr_data_i;
+              tx_cmd(15 downto 8) <= unsigned(wr_data_i);
 
             when C_TX_LENGTH =>
-              tx_length(7 downto 0) <= wr_data_i;
+              tx_length(7 downto 0) <= unsigned(wr_data_i);
 
             when C_TX_LENGTH + 1 =>
-              tx_length(15 downto 8) <= wr_data_i;
+              tx_length(15 downto 8) <= unsigned(wr_data_i);
 
             when others =>
               null;
@@ -86,28 +131,34 @@ begin
           case unsigned(addr_i) is
 
             when C_PP_PTR =>
-              rd_data_o <= pp_ptr(7 downto 0);
+              rd_data_o <= std_logic_vector(pp_ptr(7 downto 0));
 
             when C_PP_PTR + 1 =>
-              rd_data_o <= pp_ptr(15 downto 8);
+              rd_data_o <= std_logic_vector(pp_ptr(15 downto 8));
+
+            when C_PP_DATA_0 =>
+              rd_data_o <= std_logic_vector(pp_rddat(7 downto 0));
+
+            when C_PP_DATA_0 + 1 =>
+              rd_data_o <= std_logic_vector(pp_rddat(15 downto 8));
 
             when C_RXTX_REG_0 =>
-              rd_data_o <= rxtx_reg_0(7 downto 0);
+              rd_data_o <= std_logic_vector(rxtx_reg_0(7 downto 0));
 
             when C_RXTX_REG_0 + 1 =>
-              rd_data_o <= rxtx_reg_0(15 downto 8);
+              rd_data_o <= std_logic_vector(rxtx_reg_0(15 downto 8));
 
             when C_TX_CMD =>
-              rd_data_o <= tx_cmd(7 downto 0);
+              rd_data_o <= std_logic_vector(tx_cmd(7 downto 0));
 
             when C_TX_CMD + 1 =>
-              rd_data_o <= tx_cmd(15 downto 8);
+              rd_data_o <= std_logic_vector(tx_cmd(15 downto 8));
 
             when C_TX_LENGTH =>
-              rd_data_o <= tx_length(7 downto 0);
+              rd_data_o <= std_logic_vector(tx_length(7 downto 0));
 
             when C_TX_LENGTH + 1 =>
-              rd_data_o <= tx_length(15 downto 8);
+              rd_data_o <= std_logic_vector(tx_length(15 downto 8));
 
             when others =>
               null;
